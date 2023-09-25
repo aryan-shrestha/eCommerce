9d3afa360a3a1e87b43be4d01e333f60f7960f26
Aryan Shrestha <aryanshrestha183@gmail.com>
2023-09-25 17:12:20 +0545

    render config added

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 from . import views
 
@@ -16,3 +18,4 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
